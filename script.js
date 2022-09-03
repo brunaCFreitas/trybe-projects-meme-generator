0c8d821a25033b272363aa97a71e4aecaae1601a
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-02 23:05:38 -0300

    Adiciona requisito 6

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const textInput = document.getElementById('text-input');
 const containerText = document.getElementById('meme-text');
 const photoInput = document.getElementById('meme-insert');
 const imagePreview = document.getElementById('meme-image');
+const containerImage = document.getElementById('meme-image-container');
+const buttonFire = document.getElementById('fire');
+const buttonWater = document.getElementById('water');
+const buttonEarth = document.getElementById('earth');
 
 function uploadImage() {
   const reader = new FileReader();
@@ -15,5 +19,14 @@ function previewText() {
   containerText.innerText = textInput.value;
 }
 
+function onClickButton(event) {
+  const { target } = event;
+  const colorClass = target.attributes['data-class'].nodeValue;
+  containerImage.className = colorClass;
+}
+
 textInput.addEventListener('input', previewText);
 photoInput.addEventListener('input', uploadImage);
+buttonFire.addEventListener('click', onClickButton);
+buttonWater.addEventListener('click', onClickButton);
+buttonEarth.addEventListener('click', onClickButton);
